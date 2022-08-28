bf0f5b166001704a8603078f2de261633a8aaa3b
Fares Qaddoura <frqd92@gmail.com>
2022-08-28 20:16:49 +0100

    will add interface elements and change JS logic to suit it

--- script.js
+++ script.js
@@ -1,4 +1,61 @@
-let getComputerChoice= function(){
+
+
+const buttons =document.querySelectorAll(".btnPlayer");
+let userChoice = "";
+// User buttons
+function userSelection (){
+
+    if(this===buttons[0]){
+        userChoice = "rock";
+
+    }
+    if(this===buttons[1]){
+        userChoice = "paper";
+
+    }
+    if(this===buttons[2]){
+        userChoice = "scissor";
+    }  
+
+    game(userChoice);
+
+  
+}
+buttons.forEach((button) => {
+    button.addEventListener("click", userSelection);
+})
+
+function game (userC){
+let pScore = document.getElementById("player-score");
+let pScoreNum = parseInt(pScore.innerHTML);
+let cScore = document.getElementById("computer-score");
+let cScoreNum = parseInt(cScore.innerHTML);
+let randomElement = computerSelection();
+let roundScore=playRound(randomElement,userC);
+
+
+console.log("score" + roundScore);
+
+if(roundScore === 1){
+    pScoreNum++;
+    pScore.innerHTML=pScoreNum;
+}
+else if(roundScore === 2){
+    cScoreNum++;
+    cScore.innerHTML=cScoreNum;
+}
+if(pScoreNum === 5 || cScoreNum === 5){
+    let scoreDisplay = document.getElementsByClassName("results-box");
+    scoreDisplay.classList.add("colors");
+    
+}
+
+
+}
+
+
+// computer random selection
+function computerSelection(){
     let random = Math.floor(Math.random()*3);
     if(random==0){
         return "rock";
@@ -9,76 +66,49 @@ let getComputerChoice= function(){
     else if(random==2){
         return "scissor";
     }
-};    
+};  
 
+//each round
 let playRound =function(computerSelection,playerSelection){
-    if(playerSelection=="scissors"){
-        playerSelection="scissor";
-    }
+
     if(     (playerSelection=="rock" && computerSelection=="scissor")||
             (playerSelection=="paper" && computerSelection=="rock") ||
             (playerSelection=="scissor"&&computerSelection=="paper") ){  
-            return 2; 
+            return 1; 
     }
 
     else if( (computerSelection=="rock" && playerSelection=="scissor")||
              (computerSelection=="paper" && playerSelection=="rock") ||
              (computerSelection=="scissor"&&playerSelection=="paper") )  {
-
-            return 1;    
+            return 2;    
     } 
+
     else if (playerSelection==computerSelection){
-            return 0;
-    }
-    else{
-        return 4;
+            return 3;
     }
 }; 
 
+/*
+for(let x=0;x<5;x++){
 
-function game(){
-    let computerScore=0, playerScore=0, tieScore=0, msg="";
-    for(let x=0;x<30;x++){
-        let userChoice=prompt("Rock, Paper, Scissors?").toLowerCase();
-        let randomElement=getComputerChoice();
-        let roundScore=playRound(randomElement, userChoice);
-      if(roundScore==2){
-            playerScore++;
-            msg=`You win! ${userChoice} beats ${randomElement} `;
-        }
-        else if(roundScore==1){
-            computerScore++;
-            msg=`You lose! ${randomElement} beats ${userChoice}`;
-        }  
-        else if(roundScore==0){
-            tieScore++;
-            msg= (`Tie!`);
-        }
-        else if(roundScore==4){
-            msg= (`${userChoice} is not a valid weapon.\nTry again with rock, paper, or scissors`);
-        }          
-
-        alert   (
-                "Your choice: "+ userChoice
-                +"          Computer Choice: " + randomElement
-                +"\n" + msg
-                + "\nComputer score: " + computerScore
-                + "         Your Score: " + playerScore
-                +"          Ties: " + tieScore
-                );
-
-        if(playerScore==5){
-            alert("You win the whole game!");
-            break;
-        }
-        else if(computerScore==5){
-            alert("You lose the whole game!");
-            break;
-        }
-    }
-
-    alert("Final Score\nYou: " + playerScore + "\nComputer: " + computerScore);
-}
-alert("Rock.Paper.Scissors. First to 5 wins!")
-game();
-
+    if(roundScore==1){
+          playerScore++;
+          console.log(`You win! ${userC} beats ${randomElement} `);
+      }
+      else if(roundScore==2){
+          computerScore++;
+          console.log(`You lose! ${userC} beats ${userChoice}`);
+      }  
+      else if(roundScore==3){
+          //tieScore++;
+          console.log(`Tie!`);
+      }
+      if(playerScore==5){
+          console.log("You win the whole game!");
+          break;
+      }
+      else if(computerScore==5){
+          console.log("You lose the whole game!");
+          break;
+      }
+  }*/
